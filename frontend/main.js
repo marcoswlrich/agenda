8c117a5631ef866311811354a948f51ce7dc2867
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-12-30 00:43:39 -0300

    Criação e estilização página Login

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,15 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import './assets/css/style.css'
+
+const sign_in_btn = document.querySelector('#sign-in-btn')
+const sign_up_btn = document.querySelector('#sign-up-btn')
+const container = document.querySelector('.container-login')
+
+sign_up_btn.addEventListener('click', () => {
+  container.classList.add('sign-up-mode')
+})
+
+sign_in_btn.addEventListener('click', () => {
+  container.classList.remove('sign-up-mode')
+})
