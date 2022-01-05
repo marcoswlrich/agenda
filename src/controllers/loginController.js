89bbd9ab73f7c7e849258647126e1ff63ccf57d6
Marcos Wlrich <marcosapw.dev@gmail.com>
2022-01-05 17:48:51 -0300

    config register login part2 finish

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,16 +5,24 @@ exports.index = (req, res) => {
 }
 
 exports.register = async (req, res) => {
-  const login = new Login(req.body)
-  await login.register()
+  try {
+    const login = new Login(req.body)
+    await login.register()
 
-  if (login.errors.length > 0) {
-    req.flash('errors', login.errors)
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors)
+      req.session.save(function () {
+        return res.redirect('/login')
+      })
+      return
+    }
+
+    req.flash('success', 'Usuário criado com sucesso.')
     req.session.save(function () {
-      return res.redirect('back')
+      return res.redirect('/login')
     })
-    return
+  } catch (error) {
+    console.log(error)
+    return res.render('404')
   }
-
-  res.send(login.errors)
 }
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true },
-  senha: { type: String, required: true }
+  password: { type: String, required: true }
 })
 
 const LoginModel = mongoose.model('Login', LoginSchema)
@@ -20,11 +21,23 @@ class Login {
     this.validar()
     if (this.errors.length > 0) return
 
+    await this.userExists()
+
+    if (this.errors.length > 0) return
+
+    const salt = bcryptjs.genSaltSync()
+    this.body.password = bcryptjs.hashSync(this.body.password, salt)
+
     try {
       this.user = await LoginModel.create(this.body)
     } catch (error) {
       console.log(error)
     }
+  }
+
+  async userExists() {
+    const user = await LoginModel.findOne({ email: this.body.email })
+    if (user) this.errors.push('Usuário já cadastrado')
   }
 
   validar() {
