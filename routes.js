e349750a03754aee1306c00d7c0ab95240cc6a52
Marcos Wlrich <marcosapw.dev@gmail.com>
2022-01-09 01:05:17 -0300

    Estilização página contatos

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const route = express.Router()
+
 const homeController = require('./src/controllers/homeController')
 const loginController = require('./src/controllers/loginController')
+const contatoController = require('./src/controllers/contatoController')
 
 // rotas da home
 route.get('/', homeController.index)
@@ -11,4 +13,8 @@ route.get('/login', loginController.index)
 route.post('/login/register', loginController.register)
 route.post('/login/log', loginController.login)
 route.get('/login/logout', loginController.logout)
+
+//Rotas de contato
+route.get('/contato', contatoController.index)
+
 module.exports = route
