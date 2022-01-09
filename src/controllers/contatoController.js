9709e1e2e3e8b548909aac322e0df2e075fac194
Marcos Wlrich <marcosapw.dev@gmail.com>
2022-01-09 02:54:22 -0300

    Cadastrando contatos (Create)

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,3 +1,25 @@
+const Contato = require('../models/ContatoModel')
+
 exports.index = (req, res) => {
   res.render('contato')
+}
+
+exports.register = async (req, res) => {
+  try {
+    const contato = new Contato(req.body)
+    await contato.register()
+
+    if (contato.errors.length > 0) {
+      req.flash('errors', contato.errors)
+      req.session.save(() => res.redirect('/contato'))
+      return
+    }
+
+    req.flash('sucess', 'Contato registrado com sucesso')
+    req.session.save(() => res.redirect('/contato'))
+    return
+  } catch (error) {
+    console.log(error)
+    return res.render('404')
+  }
 }
--- src/models/HomeModel.js
+++ src/models/HomeModel.js
@@ -6,3 +6,6 @@ const HomeSchema = new mongoose.Schema({
 })
 
 const HomeModel = mongoose.model('Home', HomeSchema)
+
+class Home {}
+module.exports = Home
